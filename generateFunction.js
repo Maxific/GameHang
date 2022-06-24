9efb6fb24bf2a093cf8f949d19cf76ff81b03c4a
Web <web@it.step>
2022-06-24 20:47:48 +0300

    add function in another file

---
+++ generateFunction.js
@@ -0,0 +1,17 @@
+export function randomWord() {
+    word = words[Math.floor(Math.random() * words.length)];
+}
+
+export function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+      `<button id = '` + letter + `'onClick="selectGuess('` + letter + `')"> ` + letter + ` </button>`).join(' ');
+  
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+  
+}
+
+export function guessWord() {
+  wordH = word.split('').map(letter => (guess.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('word').innerHTML = wordH;
+}
